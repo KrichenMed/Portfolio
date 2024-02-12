128b6257db55792e580dd242d2c1c6b69d8a25f2
Mohamed KRICHENE <87141255+KrichenMed@users.noreply.github.com>
2024-02-12 17:06:52 +0100

    Update V1

--- p/src/App.js
+++ p/src/App.js
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Navb from "./Components/Navb";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there !!</h1>
+      <Navb />
       <Routes>
         <Route exact path="/{p}" />
       </Routes>
