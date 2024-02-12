b86de9cb230844aa1055c2df8f46981b292755f1
Mohamed KRICHENE <87141255+KrichenMed@users.noreply.github.com>
2024-02-12 16:34:59 +0100

    Update 2

--- p/src/App.js
+++ p/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import { Route, Routes } from "react-router";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there !!</h1>
+      <Routes>
+        <Route exact path="/{p}" />
+      </Routes>
     </div>
   );
 }
